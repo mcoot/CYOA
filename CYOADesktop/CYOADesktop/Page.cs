2e44ac490f0c7287a4d6bd14204cba4870902d91
JosephSpearritt <joseph.spearritt@uq.net.au>
2016-07-07 10:25:26 +1000

    Add basic page/story data structures

---
+++ CYOADesktop/CYOADesktop/Page.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYOAStructures
+{
+    class Page
+    {
+
+        private Dictionary<string, Page> branches;
+        private List<string> lines;
+        
+        public ReadOnlyDictionary<string, Page> Branches
+        {
+            get
+            {
+                return new ReadOnlyDictionary<string, Page>(branches);
+            }
+        }
+
+        public ReadOnlyCollection<string> Lines
+        {
+            get
+            {
+                return lines.AsReadOnly();
+            }
+        }
+
+        public Page()
+        {
+            branches = new Dictionary<string, Page>();
+            lines = new List<string>();
+        }
+
+        public void AppendLine(string line)
+        {
+            lines.Add(line);
+        }
+
+        public void AddBranch(string option, Page page)
+        {
+            branches.Add(option, page);
+        }
+
+        public void AddBranch(string option)
+        {
+            AddBranch(option, null);
+        }
+
+       /// <summary>
+       /// Set the page which a given option branches to
+       /// </summary>
+       /// <param name="option">The option branch to modify</param>
+       /// <param name="page">The page to branch to</param>
+       public void SetBranchPage(string option, Page page)
+        {
+            if (!(branches.ContainsKey(option))) {
+                throw new ArgumentException(
+                    String.Format("Branch with option name {0} does not exist", option), "option");
+            }
+
+            branches[option] = page;
+        }
+
+        public bool IsDeadEnd()
+        {
+            return (branches.Count <= 0);
+        }
+
+    }
+}
---
+++ CYOADesktop/CYOADesktop/Story.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYOAStructures
+{
+    class Story
+    {
+
+        private List<Page> pages;
+
+        public ReadOnlyCollection<Page> Pages
+        {
+            get
+            {
+                return pages.AsReadOnly();
+            }
+        }
+
+        public Page StartPage { get; set; }
+        public string Title { get; set; }
+
+        public Story()
+        {
+            StartPage = null;
+        }
+
+        public void AddPage(Page page)
+        {
+            pages.Add(page);
+        }
+
+    }
+}
